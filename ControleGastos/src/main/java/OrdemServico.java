44266f291a78cacec34707b8e114fd34aac4b42f
FilipeRobot0 <filexicientista@gmail.com>
2018-07-11 12:36:49 -0300

    sdge

---
+++ ControleGastos/src/main/java/OrdemServico.java
@@ -0,0 +1,47 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class OrdemServico {
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
+    private Date data;
+    private double valorTotal;
+    
+}
